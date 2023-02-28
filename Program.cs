f398831ca989be666e9924f81abda3ca458471cd
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-28 11:19:06 +0530

    Added Greeting Program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmProblems
+{
+    class Porgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Data Structures and Algorithm Problems...");
+        }
+    }
+}
