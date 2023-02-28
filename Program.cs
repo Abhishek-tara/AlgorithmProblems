67c3452613c341454e315fa5cebcc7d64b33ce7b
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-28 15:28:09 +0530

    Added method to sort an array using Insertion sort

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Data Structures and Algorithm Problems...");
+            Console.WriteLine("Function to Sort the array of string using Onserrtion sort..\n");
+
+            string[] arr = { "Abhi", "am", "I" };
+            int n = arr.Length;
+
+            InsertionSort array = new InsertionSort();
+            array.sort(arr, n);
+            array.printArraystring(arr, n);
         }
     }
 }
