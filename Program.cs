ed51f419743447846fafc6881e553dc5a459f0ab
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-01 18:49:04 +0530

    Added Merge sorting method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace AlgorithmProblems
+﻿using System;
+
+namespace AlgorithmProblems
 {
     class Porgram
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Data Structures and Algorithm Problems...");
+
+
+            int[] arr = { 8, 4, 21, 88, 45, 125 , 12 };
+
+            Console.WriteLine("Array before sorting:");
+            MergeSort.PrintArray(arr);
+
+            MergeSort.mergeSort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine("\nArray after Merge sort:");
+            MergeSort.PrintArray(arr);
         }
     }
 }
