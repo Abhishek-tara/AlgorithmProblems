67028dfbf2c6d3e9372b330263d2a89ec3fb6b7a
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-01 18:57:10 +0530

    Added Method to find Permutation of string

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Data Structures and Algorithm Problems...");
+
+            PermutationOfString permuteString = new PermutationOfString();
+
+            permuteString.Display();
         }
     }
 }
